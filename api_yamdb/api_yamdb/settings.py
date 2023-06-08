39f9eed0c3500cd688117d1805538bcfdce5952d
daycat <vladimir.orlov.2003@mail.ru>
2023-06-09 01:26:24 +0300

    settings.py changes

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -103,8 +103,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 FLUGERGEHAIMER = ('me',)
 
